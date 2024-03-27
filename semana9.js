a1b6c46b954640c0400b5668892c57e1d9b4b0d2
Matheus Silva <matheus_lima-silva@estudante.sesisenai.org.br>
2024-03-27 18:59:00 -0300

    Mensagem do commit

--- semana9.js
+++ semana9.js
@@ -1,3 +1,5 @@
+// Exercicio 1
+
 var express = require("express")
 
 var app = express();
@@ -5,6 +7,8 @@ var app = express();
 app.get("/", function(req, res){
     res.send("Pagina inicial - para ir para pagina de sobre /sobre e para ir para pagina de contato /contato")
 });
+
+// Exercicio 2
 
 app.get("/sobre", function(req, res){
     res.send("essa é a rota sobre")
@@ -14,6 +18,16 @@ app.get("/contato", function(req, res){
     res.send("essa é a rota contato")
 });
 
+// Exercicio 4
+
+app.get("/produto/:nome", function(req, res){
+    const nome = req.params.nome
+
+    
+    res.send("Seu produto é " + nome)
+});
+
 app.listen(3000, function(){
     console.log('Minha aplicação está no ar!')
 }); 
+
